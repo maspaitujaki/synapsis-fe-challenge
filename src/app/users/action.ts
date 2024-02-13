eefa53b6b69e192d191cfb11b347d8c8837e76f9
Dimas FM <dimasfaid@gmail.com>
2024-02-14 00:12:48 +0700

    feat: add delete and edit user

--- src/app/users/action.ts
+++ src/app/users/action.ts
@@ -23,6 +23,47 @@ export async function createUser(prevState: any, formData: FormData){
   const content = await rawResponse.json();
   return {
     success:true,
-    newUser: content
+    user: content
+  }
+}
+
+export async function updateUser(user_id: number, prevState: any, formData: FormData){
+  const rawFormData = {
+    name: formData.get('name'),
+    email: formData.get('email'),
+    gender: formData.get('gender'),
+    status: formData.get('status'),
+  }
+  const rawResponse = await fetch(`https://gorest.co.in/public/v2/users/${user_id}`,{
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer c37216f918615142b0667c67dd95cc3d59b0cb39731c5623e2a7c3c6fb1ef18f',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(rawFormData)
+  })
+  if (!rawResponse.ok) {
+    const content = await rawResponse.json();
+    return { success: false, error: true, message: `${content[0].field} ${content[0].message}`}
+  }
+  const content = await rawResponse.json();
+  return {
+    success:true,
+    user: content
+  }
+}
+
+export async function deleteUser(user_id: string) {
+  const rawResponse = await fetch(`https://gorest.co.in/public/v2/users/${user_id}`,{
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer c37216f918615142b0667c67dd95cc3d59b0cb39731c5623e2a7c3c6fb1ef18f',
+      'Content-Type': 'application/json',
+    },
+  })
+  if (!rawResponse.ok) {
+    throw new Error('Failed to delete user')
   }
 }
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -3,10 +3,12 @@ import Modal, { ModalBody, ModalContent, ModalFooter, ModalHead } from "@/compon
 import { ListUserSetting, User, getUserList } from "@/models/User"
 import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react"
 import Image from "next/image";
-import { createUser } from "./action";
+import { createUser, deleteUser, updateUser } from "./action";
 import { useFormState, useFormStatus } from "react-dom";
 
-function UserTableRow({user}: {user: User}) {
+function UserTableRow({user, setUsers}: {user: User, setUsers: Dispatch<SetStateAction<User[]>>}) {
+  const [open, setOpen] = useState(false);
+  
   return (
     <tr className="bg-white border-b">
       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
@@ -24,15 +26,28 @@ function UserTableRow({user}: {user: User}) {
       <td className="px-6 py-4">
         {user.status}
       </td>
-      <td className="px-6 py-4 text-right">
-        <button className="font-medium text-yellow-600 hover:underline">Edit</button>
-        <button className="font-medium text-red-600 hover:underline">Delete</button>
+      <td className="px-6 py-4 text-right space-x-2">
+        <button onClick={()=>setOpen(open=>!open)} className="font-medium text-yellow-600 hover:underline">Edit</button>
+        <button 
+          type="button" 
+          className="font-medium text-red-600 hover:underline"
+          onClick={async ()=>{
+            try {
+              await deleteUser(String(user.id))
+              setUsers(users => {
+                return users.filter((iUser) => iUser.id !== user.id)
+              })
+            } catch (error) {
+              alert('Failed to delete user')
+            }
+          }} >Delete</button>
       </td>
+      <EditUserModal open={open} setOpen={setOpen} user={user} setUsers={setUsers}/>
     </tr>
   )
 }
 
-function UserTable({users}:{users: User[]}) {
+function UserTable({users, setUsers}:{users: User[], setUsers: Dispatch<SetStateAction<User[]>>}) {
   return (
     <div className="w-full relative overflow-x-auto shadow-md sm:rounded-lg">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500">
@@ -61,7 +76,7 @@ function UserTable({users}:{users: User[]}) {
               <tbody>
                 {
                   users.map((user, idx) => (
-                    <UserTableRow key={idx} user={user}/>
+                    <UserTableRow key={idx} user={user} setUsers={setUsers}/>
                   ))
                 }
               </tbody>
@@ -73,14 +88,87 @@ const initialState = {
   success: false,
   error: false,
   message: "",
-  newUser: undefined,
+  user: undefined,
 }
+
+function EditUserModal({open, setOpen, user, setUsers}: {open:boolean, setOpen: Dispatch<SetStateAction<boolean>>, user: User, setUsers: Dispatch<SetStateAction<User[]>>}) {
+  const updateUserWithId = updateUser.bind(null, user.id)
+  const [state, formAction] = useFormState(updateUserWithId, initialState)
+  const { pending } = useFormStatus()
+  useEffect(() => {
+    if (state.user !== undefined) setUsers(users => {
+      return [state.user, ...users.filter(iUser => iUser.id !== state.user.id)]
+    });
+  }, [setUsers, state.user])
+  return (
+    <Modal open={open && !state.success} setOpen={setOpen}>
+      <ModalBody>
+        <ModalHead>
+          Edit @{user.id}&apos;s info
+        </ModalHead>
+        <ModalContent>
+          <form id='create-user-form' className="grid grid-cols-3 gap-2" action={formAction}>
+            <div className="flex items-center">
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-700">Name</label>
+            </div>
+            <input type="text" name="name" id="name" defaultValue={user.name} className="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John Doe" required/>
+            <div className="flex items-center">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-700">Email</label>
+            </div>
+            <input type="email" name="email" id="email" defaultValue={user.email} className="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@example.com" required/>
+            <div className="flex items-center">
+              <label htmlFor="gender" className=" block mb-2 text-sm font-medium text-gray-700">Gender</label>
+            </div>
+            <select defaultValue={user.gender} name="gender" className="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" id="gender">
+              <option>Select gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+            <div className="flex items-center">
+              <label htmlFor="status" className="block mb-2 text-sm font-medium text-gray-700">Status</label>
+            </div>
+            <select defaultValue={user.status} name="status" className="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" id="status">
+              <option>Select status</option>
+              <option value="active">Active</option>
+              <option value="inactive">Inactive</option>
+            </select>
+          </form>
+          <p className="text-red-400">
+            {state && state.error && state.message}
+          </p>
+        </ModalContent>
+      </ModalBody>
+      <ModalFooter>
+        <div className="flex justify-between">
+          <button
+            type="button"
+            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+            onClick={() => setOpen(false)}
+          >
+            Cancel
+          </button>
+          <button
+            disabled={pending}
+            form="create-user-form"
+            type="submit"
+            className="inline-flex w-full justify-center rounded-md bg-zinc-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-zinc-600 sm:ml-3 sm:w-auto"
+          >
+            {
+              pending? "Submitting..." : "Submit"
+            }
+          </button>
+        </div>
+      </ModalFooter>
+    </Modal>
+  )
+}
+
 function CreateUserModal({open, setOpen, setUsers}: {open:boolean, setOpen: Dispatch<SetStateAction<boolean>>, setUsers: Dispatch<SetStateAction<User[]>>}) {
   const [state, formAction] = useFormState(createUser, initialState)
   const { pending } = useFormStatus()
   useEffect(() => {
-    if (state.newUser !== undefined) setUsers(users => [state.newUser,...users]);
-  }, [setUsers, state.newUser])
+    if (state.user !== undefined) setUsers(users => [state.user,...users]);
+  }, [setUsers, state.user])
   return (
     <Modal open={open && !state.success} setOpen={setOpen}>
       <ModalBody>
@@ -279,7 +367,7 @@ export default function UsersPage() {
             </div>
         </aside>
         <main className="col-span-3">
-          <UserTable users={users}/>
+          <UserTable users={users} setUsers={setUsers}/>
         </main>
       </div>
       <CreateUserModal open={open} setOpen={setOpen} setUsers={setUsers}/>
